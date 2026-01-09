662bd8a2cd4289cd67c669c97ff14f1ddc3ddca9
Hazem Elgindy <therealhazemelgindy@gmail.com>
2026-01-10 00:26:50 +0200

    Updating About us

--- components/Numbers.tsx
+++ components/Numbers.tsx
@@ -24,10 +24,10 @@ const Numbers = () => {
         <div className="w-full bg-linear-to-t from-dark-primary to-myprimary/85">
 
             <div className='container mx-auto px-4 py-16'>
-                <div className="text-white flex flex-row justify-between items-center text-center gap-[5%]">
+                <div className="text-white flex flex-row justify-between items-start text-center gap-[4%]">
                     {cards.map((item) => (
                         <div key={item.title}>
-                            <h1 className="font-bold text-[8vw] lg:text-[3vw]">{item.title}</h1>
+                            <h1 className="font-bold text-[7.5vw] lg:text-[3vw]">{item.title}</h1>
                             <h2 className="lg:text-[1vw] leading-5 text-white/80">{item.desc}</h2>
                         </div>
                     ))}
