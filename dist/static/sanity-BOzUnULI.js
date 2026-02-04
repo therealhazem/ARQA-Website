f512c0ea20deaf4f0e301c8af748f98ce906f2e1
Hazem Elgindy <therealhazemelgindy@gmail.com>
2026-02-04 07:11:37 +0200

    Sanity Setup

---
+++ dist/static/sanity-BOzUnULI.js
@@ -0,0 +1,1 @@
+import{r as t,s as e}from"./sanity.config-CHoPHFPd.js";t(document.getElementById("sanity"),e,{reactStrictMode:!1,basePath:"/"});
---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,7 @@
+import { type SchemaTypeDefinition } from 'sanity'
+import { postType } from './postType'
+
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [postType],
+}
