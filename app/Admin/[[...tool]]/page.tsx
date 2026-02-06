0d5a55e1f6b58c42d39e188e711696cdddb4c40a
Hazem Elgindy <therealhazemelgindy@gmail.com>
2026-02-06 22:21:01 +0200

    Fixing Sanity Problems

--- app/Admin/[[...tool]]/page.tsx
+++ app/Admin/[[...tool]]/page.tsx
@@ -7,13 +7,10 @@
  * https://github.com/sanity-io/next-sanity
  */
 
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../sanity.config'
-
-export const dynamic = 'force-static'
+import StudioClient from './StudioClient'
 
 export { metadata, viewport } from 'next-sanity/studio'
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
-}
+  return <StudioClient />
+} 
