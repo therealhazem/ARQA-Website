f23d72830bcdf4ab7a2ef60b3e9f38a0f9809272
Hazem Elgindy <therealhazemelgindy@gmail.com>
2026-02-08 04:53:16 +0200

    Fix part of the Products

--- sanity/lib/getProducts.ts
+++ sanity/lib/getProducts.ts
@@ -1,13 +1,35 @@
 import { client } from "./client"
-import { ProductsQuery, FeaturedProductQuery } from "../queries/Products"
+import { FeaturedProductQuery } from "../queries/Products"
 
-export async function getProducts() {
-    return await client.fetch(ProductsQuery)
+export async function getProducts(search = "", category = "") {
+    const searchFilter = search
+        ? `(
+            name match "*${search}*" ||
+            category match "*${search}*"
+          )`
+        : ""
+    const categoryFilter = category
+        ? `category match "*${category}*"`
+        : ""
+    const filters = [searchFilter, categoryFilter].filter(Boolean)
+    const whereClause = filters.length
+        ? `&& ${filters.join(" && ")}`
+        : ""
+    const query = `
+        *[_type == "Products" ${whereClause}]{
+            _id,
+            name,
+            category,
+            features,
+            images[]{ "url": asset->url }
+        }
+    `
+    return await client.fetch(query)
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function getSpecificProduct(query: any) {
-    return await client.fetch(query);
+    return await client.fetch(query)
 }
 
 export async function getFeaturedProducts() {
